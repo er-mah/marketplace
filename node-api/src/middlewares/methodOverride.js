2365a8351e8364b709c3680426ad98c3da1659ba
Esteban Rivas <estebanarivasv@gmail.com>
2023-03-20 21:32:29 -0300

    refactor: delete middlewares because express implements them

--- node-api/src/middlewares/methodOverride.js
+++
@@ -1,6 +0,0 @@
-const methodOverride = require("method-override");
-
-/* Middleware that allows HTTP methods other than GET and POST to be used in HTTP requests. */
-export function methodOverrideMdw() {
-    return methodOverride();
-}
