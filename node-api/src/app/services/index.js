01fe0ed929729482691e538430796a48258fe7fd
Esteban Rivas <estebanarivasv@gmail.com>
2023-04-05 15:36:35 -0300

    feat: add services

--- node-api/src/app/services/index.js
+++ node-api/src/app/services/index.js
@@ -1,4 +1,7 @@
 // TODO: REFACTOR CONTROLLERS
 import {InfoAutoService} from "./integrations/infoAutoService.js";
+import {PublicationService} from "./publicationService.js";
+import {PublicationChangeService} from "./publicationChangeService.js";
 
-export {InfoAutoService}
+
+export {InfoAutoService, PublicationService, PublicationChangeService}
