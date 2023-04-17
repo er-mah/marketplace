8c0f8e934e7d5202fe57a67bd6ceae72bc344034
Esteban Rivas <estebanarivasv@gmail.com>
2023-04-17 19:01:53 -0300

    feat: minor file changes

--- react-web/webpack.config.js
+++ react-web/webpack.config.js
@@ -1,8 +1,12 @@
+const Dotenv = require("dotenv-webpack");
+
 module.exports = {
   resolve: {
     fallback: {
       crypto: false,
       stream: false,
+      os: false,
     },
   },
+  plugins: [new Dotenv()],
 };
