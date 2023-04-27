9647e10008d288f64a58ea33933db9aea76584c0
Esteban Rivas <estebanarivasv@gmail.com>
2023-04-27 20:30:20 -0300

    feat: add some routes

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "flowbite";
 
 import { Auth, NotFoundPage, HomePage } from "./pages";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 import { DashboardPage } from "./pages/Dashboard.tsx";
+import VerifyAccountPage from "./pages/user/VerifyAccount.tsx";
 
 class App extends Component {
   render() {
@@ -14,7 +21,9 @@ class App extends Component {
           <Route path="/" element={<HomePage />} />
           <Route path={"/auth/*"} element={<Auth />} />
           <Route path={"/dashboard"} element={<DashboardPage />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/cuenta/verificar" element={<VerifyAccountPage />} />
+          <Route path="/not-found" element={<NotFoundPage />} />
+          <Route path="*" element={<Navigate to={"/not-found"} />} />
         </Routes>
       </Router>
     );
