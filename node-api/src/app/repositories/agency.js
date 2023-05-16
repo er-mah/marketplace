eaf5b7305ad88c5055431e4adedaea516a27c87a
Esteban Rivas <estebanarivasv@gmail.com>
2023-05-16 06:43:31 -0300

    feat: add agency search

--- node-api/src/app/repositories/agency.js
+++ node-api/src/app/repositories/agency.js
@@ -1,8 +1,18 @@
 import { AgencyModel } from "../models/index.js";
+import {Op} from "sequelize";
 
 export class AgencyRepository {
   constructor() {
     this.model = AgencyModel;
+  }
+
+  searchAgenciesByName(nameQuery) {
+    return this.model.findAll({
+      where: {
+        name: { [Op.like]: `%${nameQuery}%` },
+        email: { [Op.like]: `%${nameQuery}%` },
+      },
+    });
   }
 
   createAgency(name, address, email, phone, locality_id) {
