fd6cf8706bb67daee7a874b3f421bd65a1c3f8b0
Esteban Rivas <estebanarivasv@gmail.com>
2023-05-16 06:54:08 -0300

    refactor: todo finish auth

--- react-web/src/routes.js
+++ react-web/src/routes.js
@@ -1,11 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { NotFoundPage, HomePage } from "./pages";
 import { DashboardPage } from "./pages/Dashboard.tsx";
-import VerifyAccountPage from "./pages/user/VerifyAccount.tsx";
-import LoginPage from "./pages/auth/Login";
-import RegisterPage from "./pages/auth/Register";
-import RecoverPasswordPage from "./pages/user/RecoverPassword";
-import SetNewPasswordPage from "./pages/user/SetNewPassword";
+import { VerifyAccountPage } from "./pages/user/VerifyAccount.tsx";
+import { LoginPage } from "./pages/auth/Login";
+import { RegisterPage } from "./pages/auth/Register";
+import { RecoverPasswordPage } from "./pages/user/RecoverPassword";
+import { SetNewPasswordPage } from "./pages/user/SetNewPassword";
+import { AddUserAdditionalInfoPage } from "./pages/user/AddAdditionalInfo";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const routes = [
   { path: "/", element: <HomePage /> },
@@ -18,14 +20,18 @@ const routes = [
       { path: "*", element: <Navigate to="/auth" replace /> },
     ],
   },
-  { path: "/dashboard", element: <DashboardPage /> },
+  { path: "/dashboard", element: <ProtectedRoute component={DashboardPage} />, },
   { path: "/cuenta/verificar", element: <VerifyAccountPage /> },
   { path: "/cuenta/recuperar", element: <RecoverPasswordPage /> },
   { path: "/cuenta/nuevas-credenciales", element: <SetNewPasswordPage /> },
+  {
+    path: "/cuenta/agregar-informacion-adicional",
+    //element: <ProtectedRoute component={AddUserAdditionalInfoPage} />,
+    element: <AddUserAdditionalInfoPage />,
+  },
   { path: "/not-found", element: <NotFoundPage /> },
   { path: "*", element: <Navigate to={"/not-found"} replace /> },
   { path: "/terminos-y-condiciones", element: <NotFoundPage /> },
-
 ];
 
 export default routes;
