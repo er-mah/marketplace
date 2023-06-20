ccfde431e4d12bdf9778a12c615b42fb3b568dbc
Esteban <estebanarivasv@gmail.com>
2023-06-20 14:31:13 -0300

    feat: update routes

--- react-web/src/routes.js
+++ react-web/src/routes.js
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { NotFoundPage, HomePage } from "./pages";
-import { DashboardPage } from "./pages/Dashboard.tsx";
+import { Dashboard } from "./pages/Dashboard.jsx";
 import { VerifyAccountPage } from "./pages/user/VerifyAccount.tsx";
 import { LoginPage } from "./pages/auth/Login";
 import { RegisterPage } from "./pages/auth/Register";
@@ -8,6 +8,9 @@ import { RecoverPasswordPage } from "./pages/user/RecoverPassword";
 import { SetNewPasswordPage } from "./pages/user/SetNewPassword";
 import { AddUserAdditionalInfoPage } from "./pages/user/AddAdditionalInfo";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import {UserPublications} from "./pages/dashboard/UserPublications";
+import {Profile} from "./pages/dashboard/Profile";
+import {CreatePublication} from "./pages/dashboard/CreatePublication";
 
 const routes = [
   { path: "/", element: <HomePage /> },
@@ -20,7 +23,19 @@ const routes = [
       { path: "*", element: <Navigate to="/auth" replace /> },
     ],
   },
-  { path: "/dashboard", element: <ProtectedRoute component={DashboardPage} />, },
+  {
+    path: "/dashboard/*",
+    children: [
+      { path: "", element: <Navigate to="/dashboard/mis-publicaciones" /> },
+      { path: "mis-publicaciones", element: <Dashboard ChildComponent={UserPublications} /> },
+      { path: "mi-perfil", element: <Dashboard ChildComponent={Profile} /> },
+      {
+        path: "crear-publication",
+        element: <Dashboard ChildComponent={CreatePublication} />,
+      },
+      { path: "*", element: <Navigate to="/auth" replace /> },
+    ],
+  },
   { path: "/cuenta/verificar", element: <VerifyAccountPage /> },
   { path: "/cuenta/recuperar", element: <RecoverPasswordPage /> },
   { path: "/cuenta/nuevas-credenciales", element: <SetNewPasswordPage /> },
