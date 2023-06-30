18a7e5e9158d8d156cc804a48a2e8f8acfa4743e
Esteban <estebanarivasv@gmail.com>
2023-06-30 01:30:09 -0300

    feat: update guard

--- react-web/src/components/ProtectedRoute.jsx
+++ react-web/src/components/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AuthCacheManager } from "../apollo/authCacheManager";
 import { Navigate, Route, useNavigate } from "react-router-dom";
 import { fullPaths } from "../utils/routesConstants";
+import {toast, ToastContainer} from "react-toastify";
 
 const authCacheManager = new AuthCacheManager();
 
@@ -25,6 +26,16 @@ export const ProtectedRoute = ({ component: Component, ...rest }) => {
   const checkAuthentication = async () => {
     const authenticated = await isAuthenticated();
     if (!authenticated) {
+      await toast.error("Tenés que estar autenticado para acceder a este recurso", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       navigate(fullPaths.login); // Redirige a la página de inicio de sesión si el usuario no está autenticado
     }
   };
@@ -33,5 +44,8 @@ export const ProtectedRoute = ({ component: Component, ...rest }) => {
     checkAuthentication();
   });
 
-  return <Component />;
+  return <>
+    <ToastContainer />
+    <Component />
+  </>;
 };
